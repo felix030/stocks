ce98855bbf20021bb70cf2f255a4833bc942f17e
Felix Klemke <felix.klemke@trifork.nl>
2021-11-20 13:48:44 +0100

    T-10: Add initial validation to store stocks

--- src/main/java/com/felixklemke/stocks/components/StockService.java
+++ src/main/java/com/felixklemke/stocks/components/StockService.java
@@ -3,6 +3,7 @@ package com.felixklemke.stocks.components;
 import com.felixklemke.stocks.model.Stock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
@@ -10,11 +11,13 @@ import javax.validation.Valid;
 @Service
 @Validated
 @RequiredArgsConstructor
+@Transactional
 public class StockService {
 
     private final StockRepository repository;
 
     public Stock saveStock(@Valid Stock stock) {
+        System.err.println();
         return repository.save(stock);
     }
 }
--- src/test/java/com/felixklemke/stocks/StockServiceTest.java
+++ src/test/java/com/felixklemke/stocks/StockServiceTest.java
@@ -2,10 +2,16 @@ package com.felixklemke.stocks;
 
 import com.felixklemke.stocks.components.StockService;
 import com.felixklemke.stocks.model.Stock;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import javax.validation.ConstraintViolationException;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class StockServiceTest extends AbstractIntegrationTest {
@@ -13,15 +19,25 @@ class StockServiceTest extends AbstractIntegrationTest {
     @Autowired
     StockService stockService;
 
-    @Test
-    void givenInvalidStockWhenSavedThenThrowException() {
+    @ParameterizedTest(name = "{index} Given invalid stock properties for {0}")
+    @MethodSource("invalidStockArguments")
+    void givenInvalidStockWhenSavedThenThrowException(String testCase, String stockName, UUID externalId, Long currentPrice) {
         var stock = Stock.builder()
-                .name("")
-                .externalId(null)
-                .currentPrice(-121312L)
+                .name(stockName)
+                .externalId(externalId)
+                .currentPrice(currentPrice)
                 .build();
 
-        Stock save = stockService.saveStock(stock);
-        assertThat(save.getName()).isEqualTo(stock.getName());
+
+        assertThrows(ConstraintViolationException.class, () -> stockService.saveStock(stock));
+    }
+
+    private static Stream<Arguments> invalidStockArguments() {
+        return Stream.of(
+                Arguments.of("name", "", UUID.randomUUID(), 1L),
+                Arguments.of("currentPrice", "Test stock name", UUID.randomUUID(), -1L),
+                Arguments.of("currentPrice", "Test stock name", UUID.randomUUID(), null),
+                Arguments.of("externalId", "Test stock name", null, 1L)
+        );
     }
 }
